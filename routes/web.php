b8e86d6d26106ee1c1842c8c1f596157142cde6b
Noor muhammed <noormuhammed12345678@gmail.com>
2024-04-11 12:00:37 +0530

    added comments in web.php

--- routes/web.php
+++ routes/web.php
@@ -50,14 +50,14 @@ Route::get('/welcome', function () {
 
 
 //middle ware 
-Route::middleware(['IsAdmin'])->group(function () {
-         Route::prefix('customers')->group(function () {
-        Route::get('/', [CustomerController::class, 'index'])->name('customer.index');
-        Route::get('/form', [CustomerController::class, 'form'])->name('customer.create');
-        Route::post('/create', [CustomerController::class, 'store'])->name('customer.store');
-        Route::get('/{id}',[CustomerController::class,'edit'])->name('customer.edit');
-        Route::post('/{id}/update',[CustomerController::class,'update'])->name('customer.update');
-        Route::get('/{id}/delete',[CustomerController::class,'delete']);
+ Route::middleware(['IsAdmin'])->group(function () {
+ Route::prefix('customers')->group(function () {
+ Route::get('/', [CustomerController::class, 'index'])->name('customer.index');
+ Route::get('/form', [CustomerController::class, 'form'])->name('customer.create');
+ Route::post('/create', [CustomerController::class, 'store'])->name('customer.store');
+ Route::get('/{id}',[CustomerController::class,'edit'])->name('customer.edit');
+ Route::post('/{id}/update',[CustomerController::class,'update'])->name('customer.update');
+ Route::get('/{id}/delete',[CustomerController::class,'delete']);
     });
 //admin  product
 Route::get('/product', [ProductController::class, 'index'])->name('products.index');
@@ -75,20 +75,22 @@ Route::post('/product/{id}/update',[ProductController::class,'update'])->name('p
   Route::get('/{id}',[CategoryController::class,'edit'])->name('category.edit');
   Route::post('/{id}/update',[CategoryController::class,'update'])->name('category.update');
   Route::get('/{id}/delete',[CategoryController::class,'delete'])->name('category.delete');
-});
-
-//orders
-Route::get('/orders',[OrderController::class,'index'])->name('order.index');
- Route::get('/orders/{id}',[OrderController::class,'show'])->name('order.view'); // Corrected route definition
+  });
 
 
+ //group of orders
+ Route:prefix('orders')->group(function(){
+ //orders
+ Route::get('/',[OrderController::class,'index'])->name('order.index');
+ Route::get('/{id}',[OrderController::class,'show'])->name('order.view'); // Corrected route definition
  //change status ---new
-Route::post('/orders/updatestatus/{id}',[OrderController::class,'changeStatus'])->name('update.status');
+ Route::post('/updatestatus/{id}',[OrderController::class,'changeStatus'])->name('update.status');
 
-});
+ });
+ });
 
 
-
+//logout,login and signup for adminpage
 Route::get('/logout', function (Request $request) {
 $request->session()->forget('admin');
 return view('login');
@@ -97,12 +99,20 @@ Route::get('/showsignup',[AuthController::class,'showsignup'])->name('signuppage
 Route::post('/signup',[AuthController::class,'signup'])->name('signup');
 Route::get('/loginpage',[AuthController::class,'loginpage'])->name('loginpage');
 Route::post('/login',[AuthController::class,'login'])->name('login');
+
+
+
+//route to display all the session
 Route::get('/test', function(Request $request){
-    dd($request->session()->all());
+ dd($request->session()->all());
 });
+
+
+
+//admin message
 Route::get('/message',function(){
-    $message = message::all();
-    return view('message',['message'=>$message]);
+$message = message::all();
+return view('message',['message'=>$message]);
 });
 
 
@@ -125,86 +135,81 @@ Route::get('/message',function(){
 
 
 
-//userloginandsignup
+//user logout ,loginandsignup
 Route::get('/userloginpage',[UserController::class,'showLoginpage'])->name('userloginpage');
 Route::post('/userlogin',[UserController::class,'userLogin'])->name('userlogin');
 Route::get('/usersignuppage',[UserController::class,'userSignuppage'])->name('usersignuppage');
 Route::post('/usersignup',[UserController::class,'usersignup'])->name('usersignup');
+Route::get('/logoutuser', function (Request $request) {
+$request->session()->forget('user');
+return view('userlogin');
+})->name('userlogout');
 
 
 
-//middleware for user
- Route::middleware('IsUser')->group( function(){
-//user pages
- Route::get('/home', function () {
-     $products = Product::where('status', 0)->get(); 
-    return view('user.userhome', ['products' => $products]);
- })->name('home');
- Route::get('/categories', function () {
-    // Retrieve all categories from the database
-     $categories = categories::all(); 
-    return view('user.categories', ['categories' => $categories]);
+
+   //middleware for user
+Route::middleware('IsUser')->group( function(){
+//user home,categories,contactus,product pages
+Route::get('/home', function () {
+$products = Product::where('status', 0)->get(); 
+return view('user.userhome', ['products' => $products]);
+})->name('home');
+Route::get('/categories', function () {
+$categories = categories::all(); 
+return view('user.categories', ['categories' => $categories]);
 })->name('categories');
- Route::get('/contactus',function(){
-    return view('user.contactus');
-    })->name('contactus');
- Route::get('/products',function(request $request){
-      $products = Product::where('status', 0)->get();
-      $user = $request->session()->get('user');
-    return view('user.products',['products'=>$products,'user' => $user]);
-     })->name('products');
-
-    
-    
-    
-    
-  
-    Route::get('/cart', function (Request $request) {
-    $user = session('user');
-    // Fetch only the cart items associated with the logged-in user
-    $cart = Cart::where('userid', $user)->get();
-    return view('user.cart', ['cart' => $cart,'user' => $user]);
-    });
+Route::get('/contactus',function(){
+return view('user.contactus');
+})->name('contactus');
+Route::get('/products',function(request $request){
+$products = Product::where('status', 0)->get();
+$user = $request->session()->get('user');
+return view('user.products',['products'=>$products,'user' => $user]);
+})->name('products');
+Route::get('/cart', function (Request $request) {
+$user = session('user');
+$cart = Cart::where('userid', $user)->get();
+return view('user.cart', ['cart' => $cart,'user' => $user]);
+});
 
 
 
-   Route::get('/logoutuser', function (Request $request) {
-    $request->session()->forget('user');
-    return view('userlogin');
-    })->name('userlogout');
 
-//action controller for cart and payment
-    Route::post('/cart',[ActionController::class,'cart'])->name('cart');
-    Route::post('/contactus/submit', [ActionController::class,'submitForm'])->name('submitform');
-    Route::post('cart/delete',[ActionController::class,'deleteCart'])->name('deletecart');
-    Route::post('/update-quantity', [ActionController::class,'updateQuantity'])->name('update-quantity');
-    Route::get('cart/checkout', [ActionController::class, 'checkout'])->name('checkout');
-    Route::post('/buynow/checkout',[ActionController::class,'buynowCheckout'])->name('buynow.checkout');
+
+//action controller for actions used in cart,contactus page
+Route::post('/cart',[ActionController::class,'cart'])->name('cart');
+Route::post('/contactus/submit', [ActionController::class,'submitForm'])->name('submitform');
+Route::post('cart/delete',[ActionController::class,'deleteCart'])->name('deletecart');
+Route::post('/update-quantity', [ActionController::class,'updateQuantity'])->name('update-quantity');
+Route::get('cart/checkout', [ActionController::class, 'checkout'])->name('checkout');
+Route::post('/buynow/checkout',[ActionController::class,'buynowCheckout'])->name('buynow.checkout');
  
 
 
+//paymentcontroller used to manage payments in buynow and cart
+//buynow payment process
+Route::post('/buynow-payment-process',[PaymentController::class,'buynowpaymentProcess'])->name('buynowpayment.process');
+Route::post('/payment-process',[PaymentController::class,'paymentProcess'])->name('payment.process');
+Route::get('/payment-success',[PaymentController::class,'paymentSuccess'])->name('payment.success');
 
-    //buynow payment process
-    Route::post('/buynow-payment-process',[PaymentController::class,'buynowpaymentProcess'])->name('buynowpayment.process');
 
-    Route::post('/payment-process',[PaymentController::class,'paymentProcess'])->name('payment.process');
-    Route::get('/payment-success',[PaymentController::class,'paymentSuccess'])->name('payment.success');
 
-//user ordr and vieww page
-     Route::get('/order',[OrderstatusController::class,'index'])->name('user.orders');
-    Route::get('/orderview/{id}',[OrderstatusController::class,'show'])->name('user.view');
+//orderstatuscontroller for manage orderview and indexpage
+//user order and view page
+Route::get('/order',[OrderstatusController::class,'index'])->name('user.orders');
+Route::get('/orderview/{id}',[OrderstatusController::class,'show'])->name('user.view');
 
 
 
    
     
-    //cart count
-Route::get('/cart/count', function (Request $request) {
-    $user = $request->session()->get('user');
-    
-    $cartCount = Cart::where('userid', $user)->count(); // Assuming you have a method to get the cart count
-    
-    return response()->json(['count' => $cartCount]);
+ //cart count
+ Route::get('/cart/count', function (Request $request) {
+ $user = $request->session()->get('user');
+ $cartCount = Cart::where('userid', $user)->count(); // Assuming you have a method to get the cart count
+ //response sent through ajax
+return response()->json(['count' => $cartCount]);
 })->name('cart.count');
 
 });
